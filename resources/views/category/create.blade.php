9476fdbe35e32e9b13da28039ecde123c52cdb40
Mifta Chowdhury <miftachy79@gmail.com>
2022-11-14 14:34:02 +0600

    update

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 @section('content')
 
-<form action={{route('create.store')}} class="col-lg-8 offset-3" method='post'>
+<form action={{route('category.store')}} class="col-lg-8 offset-3" method='post'>
     @csrf
     <div class="card">
         <div class="card-header"><strong>Add New Category</strong>
