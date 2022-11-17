5268d32e92b99ce0c309dc9cd578befb08bcabc4
harunurR <harunurrashid97.ctg@gmail.com>
2022-11-17 13:35:35 +0600

    update

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,16 +3,15 @@
 @section('content')
 
 <div class="card-body">
-    <a class="btn btn-sm btn-primary float-right m-3" href="{{ route('category.create') }}">Add New</a>
+    <!-- <a class="btn btn-sm btn-primary float-right m-3" href="{{ route('products.create') }}">Add New</a> -->
     <table class="table">
         <thead>
             <tr>
                 <th scope="col">#SL</th>
-                <th scope="col">Category Name</th>
-                <th scope="col">For Games?</th>
-                <th scope="col">Featured?</th>
-                <th scope="col">Show Category Page?</th>
-                <th scope="col">Category Page Order</th>
+                <th scope="col">name</th>
+                <th scope="col">SKU</th>
+                <th scope="col">Price</th>
+                <th scope="col">Image</th>
                 <th scope="col">Action</th>
                 
             </tr>
@@ -20,36 +19,33 @@
 
 
         <tbody>
-
-            @forelse($categories as $cat)
+        @forelse($product as $p)
             <tr>
-                <th scope="row">{{++$loop->index }}</th>
-                <td>{{$cat->name}}</td>
-                <td>{{$cat->is_game}}</td>
-                <td>{{$cat->feature_cat}}</td>
-                <td>{{$cat->show_catpage}}</td>
-                <td>{{$cat->cat_page_order}}</td>
-                <td>{{$cat->cat_page_order}}</td>
+            <th scope="row">{{ ++$loop->index }}</th>
+                <td>{{$p->name}}</td>
+                <td>{{$p->sku}}</td>
+                <td>{{$p->price}}</td>
+                <td><img width="50px" src="{{asset('uploads/'.$p->image)}}" alt="">{{$p->image}}</td>
                 <td class="white-space-nowrap">
-                    <a href="{{route('category.edit',$cat->id)}}">
-                        <i class="fas fa-edit"></i>
+                    <a href="{{route('products.edit',$p->id)}}">
+                        <i class="bi bi-pencil-square"></i>
                     </a>
-                    <a href="javascript:void()" onclick="$('#form{{$cat->id}}').submit()">
-                        <i class="fas fa-trash"></i>
+                    <a href="javascript:void()" onclick="$('#form{{$p->id}}').submit()">
+                        <i class="bi bi-trash"></i>
                     </a>
-                    <form id="form{{$cat->id}}" action="{{route('category.destroy',$cat->id)}}" method="post">
+                    <form id="form{{$p->id}}" action="{{route('products.destroy',$p->id)}}" method="post">
                         @csrf
                         @method('delete')
                         
                     </form>
                 </td>
             </tr>
-           @empty
-
-           <tr>
-            <th colspan="4">No Category Found</th>
-        </tr>
-    @endforelse
+            @empty
+            <tr>
+                <th colspan="8" class="text-center">No Pruduct Found</th>
+            </tr>
+            @endforelse
+  
         </tbody>
     </table>
 </div>
