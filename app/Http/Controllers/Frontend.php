a401c4985484f344963a06ed478fb51250194191
hmarifrazi <hmarif.siblee@gmail.com>
2022-12-20 03:04:45 +0600

    ok

--- app/Http/Controllers/Frontend.php
+++ app/Http/Controllers/Frontend.php
@@ -19,211 +19,222 @@ use App\Models\CorporateInquiry;
 use App\Http\Traits\ResponseTrait;
 
 
-class Frontend extends Controller{
+class Frontend extends Controller
+{
     use ResponseTrait;
-    public function index(){
-        $slide = Slide::where('type',1)->orderBy('id','DESC')->get();
-        $category = Category::where('feature_cat','1')->orderBy('id','DESC')->limit(6)->get();
+    public function index()
+    {
+        $slide = Slide::where('type', 1)->orderBy('id', 'DESC')->get();
+        $category = Category::where('feature_cat', '1')->orderBy('id', 'DESC')->limit(6)->get();
         $manufacturer = Manufacturer::get();
-        return view('frontend.front',compact('slide','category','manufacturer'));
+        return view('frontend.front', compact('slide', 'category', 'manufacturer'));
     }
 
-    public function games(){
-        $slide = Slide::where('type',3)->orderBy('id','DESC')->get();
-        $categories = Category::where('is_game','1')->get();
-        $category = Category::where('is_game','1')->pluck('id');
+    public function games()
+    {
+        $slide = Slide::where('type', 3)->orderBy('id', 'DESC')->get();
+        $categories = Category::where('is_game', '1')->get();
+        $category = Category::where('is_game', '1')->pluck('id');
         $gamsetting = GamingSetting::first();
-        return view('frontend.games',compact('slide','category','categories','gamsetting'));
+        return view('frontend.games', compact('slide', 'category', 'categories', 'gamsetting'));
     }
 
-    public function category(){
-        $slide = Slide::where('type',2)->orderBy('id','DESC')->get();
-        $category = Category::where('show_catpage',1)->orderBy('cat_page_order','DESC')->get();
-        $categoryids = Category::where('is_game','0')->pluck('id');
+    public function category()
+    {
+        $slide = Slide::where('type', 2)->orderBy('id', 'DESC')->get();
+        $category = Category::where('show_catpage', 1)->orderBy('cat_page_order', 'DESC')->get();
+        $categoryids = Category::where('is_game', '0')->pluck('id');
         $catsetting = CategorySetting::first();
-        return view('frontend.category',compact('slide','category','catsetting','categoryids'));
+        return view('frontend.category', compact('slide', 'category', 'catsetting', 'categoryids'));
     }
 
-    public function categoryview(Request $r,$id){
-        $modelFilt=array();
-        $subcatFilt=array();
-        $manuFilt=array();
-        if(isset($r->model))
-            $modelFilt=explode(',',$r->model);
-        if(isset($r->subcat))
-            $subcatFilt=explode(',',$r->subcat);
-        if(isset($r->manu))
-            $manuFilt=explode(',',$r->manu);
+    public function categoryview(Request $r, $id)
+    {
+        $modelFilt = array();
+        $subcatFilt = array();
+        $manuFilt = array();
+        if (isset($r->model))
+            $modelFilt = explode(',', $r->model);
+        if (isset($r->subcat))
+            $subcatFilt = explode(',', $r->subcat);
+        if (isset($r->manu))
+            $manuFilt = explode(',', $r->manu);
 
-        $subcats = Subcategory::where('category_id',$id)->get();
+        $subcats = Subcategory::where('category_id', $id)->get();
         $categorynam = Category::find($id);
-        $slide=Categoryimage::where('category_id',$id)->get();
+        $slide = Categoryimage::where('category_id', $id)->get();
 
-        $products = Product::where('category_id',$id);
+        $products = Product::where('category_id', $id);
 
         /* get manufacturer*/
-        $menuids=$products->pluck('manufacturer_id');
-        $manufacturer = Manufacturer::whereIn('id',$menuids)->get();
+        $menuids = $products->pluck('manufacturer_id');
+        $manufacturer = Manufacturer::whereIn('id', $menuids)->get();
 
         /* model no */
-        $models=$products->pluck('model_no','model_no');
+        $models = $products->pluck('model_no', 'model_no');
         /* filters */
-        if(count($modelFilt) > 0){
-            $products = $products->whereIn('model_no',$modelFilt);
+        if (count($modelFilt) > 0) {
+            $products = $products->whereIn('model_no', $modelFilt);
         }
-        if(count($subcatFilt) > 0){
-            $products = $products->whereIn('subcategory_id',$subcatFilt);
+        if (count($subcatFilt) > 0) {
+            $products = $products->whereIn('subcategory_id', $subcatFilt);
         }
-        if(count($manuFilt) > 0){
-            $products = $products->whereIn('manufacturer_id',$manuFilt);
+        if (count($manuFilt) > 0) {
+            $products = $products->whereIn('manufacturer_id', $manuFilt);
         }
 
         $products = $products->paginate(20);
 
-       return view('frontend.categoryview',compact('subcats','categorynam','slide','products','manufacturer','models','modelFilt','subcatFilt','manuFilt'));
+        return view('frontend.categoryview', compact('subcats', 'categorynam', 'slide', 'products', 'manufacturer', 'models', 'modelFilt', 'subcatFilt', 'manuFilt'));
     }
-    
-    public function subcategoryview(Request $r,$id){
-        $modelFilt=array();
-        $manuFilt=array();
-        if(isset($r->model))
-            $modelFilt=explode(',',$r->model);
-        if(isset($r->manu))
-            $manuFilt=explode(',',$r->manu);
-            
+
+    public function subcategoryview(Request $r, $id)
+    {
+        $modelFilt = array();
+        $manuFilt = array();
+        if (isset($r->model))
+            $modelFilt = explode(',', $r->model);
+        if (isset($r->manu))
+            $manuFilt = explode(',', $r->manu);
+
         $subcat = Subcategory::find($id);
-        $slide=Categoryimage::where('category_id',$subcat->category_id)->get();
+        $slide = Categoryimage::where('category_id', $subcat->category_id)->get();
 
-        $products = Product::where('subcategory_id',$id);
+        $products = Product::where('subcategory_id', $id);
 
         /* get manufacturer*/
-        $menuids=$products->pluck('manufacturer_id');
-        $manufacturer = Manufacturer::whereIn('id',$menuids)->get();
+        $menuids = $products->pluck('manufacturer_id');
+        $manufacturer = Manufacturer::whereIn('id', $menuids)->get();
 
         /* model no */
-        $models=$products->pluck('model_no','model_no');
+        $models = $products->pluck('model_no', 'model_no');
         /* filters */
-        if(count($modelFilt) > 0){
-            $products = $products->whereIn('model_no',$modelFilt);
+        if (count($modelFilt) > 0) {
+            $products = $products->whereIn('model_no', $modelFilt);
         }
-        if(count($manuFilt) > 0){
-            $products = $products->whereIn('manufacturer_id',$manuFilt);
+        if (count($manuFilt) > 0) {
+            $products = $products->whereIn('manufacturer_id', $manuFilt);
         }
 
         $products = $products->paginate(20);
 
-       return view('frontend.subcategoryview',compact('subcat','slide','products','manufacturer','models','modelFilt','manuFilt'));
+        return view('frontend.subcategoryview', compact('subcat', 'slide', 'products', 'manufacturer', 'models', 'modelFilt', 'manuFilt'));
     }
-    
-    public function shopview(Request $r){
-        $modelFilt=array();
-        $subcatFilt=array();
-        $manuFilt=array();
-        if(isset($r->model))
-            $modelFilt=explode(',',$r->model);
-        if(isset($r->subcat))
-            $subcatFilt=explode(',',$r->subcat);
-        if(isset($r->manu))
-            $manuFilt=explode(',',$r->manu);
 
-        
+    public function shopview(Request $r)
+    {
+        $modelFilt = array();
+        $subcatFilt = array();
+        $manuFilt = array();
+        if (isset($r->model))
+            $modelFilt = explode(',', $r->model);
+        if (isset($r->subcat))
+            $subcatFilt = explode(',', $r->subcat);
+        if (isset($r->manu))
+            $manuFilt = explode(',', $r->manu);
+
+
         $cats = Category::orderBy('name')->get();
-        $slide = Slide::where('type',4)->orderBy('id','DESC')->get();
+        $slide = Slide::where('type', 4)->orderBy('id', 'DESC')->get();
 
-        $products = Product::where('name','!=','');
+        $products = Product::where('name', '!=', '');
 
         /* get manufacturer*/
-        $menuids=$products->pluck('manufacturer_id');
-        $manufacturer = Manufacturer::whereIn('id',$menuids)->get();
+        $menuids = $products->pluck('manufacturer_id');
+        $manufacturer = Manufacturer::whereIn('id', $menuids)->get();
 
         /* model no */
-        $models=$products->pluck('model_no','model_no');
+        $models = $products->pluck('model_no', 'model_no');
         /* filters */
-        if(count($modelFilt) > 0){
-            $products = $products->whereIn('model_no',$modelFilt);
+        if (count($modelFilt) > 0) {
+            $products = $products->whereIn('model_no', $modelFilt);
         }
-        if(count($subcatFilt) > 0){
-            $products = $products->whereIn('category_id',$subcatFilt);
+        if (count($subcatFilt) > 0) {
+            $products = $products->whereIn('category_id', $subcatFilt);
         }
-        if(count($manuFilt) > 0){
-            $products = $products->whereIn('manufacturer_id',$manuFilt);
+        if (count($manuFilt) > 0) {
+            $products = $products->whereIn('manufacturer_id', $manuFilt);
         }
 
         $products = $products->paginate(20);
 
-       return view('frontend.shopview',compact('slide','cats','products','manufacturer','models','modelFilt','subcatFilt','manuFilt'));
+        return view('frontend.shopview', compact('slide', 'cats', 'products', 'manufacturer', 'models', 'modelFilt', 'subcatFilt', 'manuFilt'));
     }
 
-    public function aboutus(){
+    public function aboutus()
+    {
         $about = AboutUsSetting::first();
-        return view('frontend.aboutus',compact('about'));
+        return view('frontend.aboutus', compact('about'));
     }
-    
-    public function contactus(){
+
+    public function contactus()
+    {
         $about = AboutUsSetting::first();
-        return view('frontend.contactus',compact('about'));
+        return view('frontend.contactus', compact('about'));
     }
-    
-    public function corporatebusiness(){
-        $slide = Slide::where('type',5)->orderBy('id','DESC')->get();
-        $data = CorporateSettings::where('status',1)->orderBy('order_no')->get();
-        return view('frontend.corporate-business',compact('slide','data'));
+
+    public function corporatebusiness()
+    {
+        $slide = Slide::where('type', 5)->orderBy('id', 'DESC')->get();
+        $data = CorporateSettings::where('status', 1)->orderBy('order_no')->get();
+        return view('frontend.corporate-business', compact('slide', 'data'));
     }
-    
-    public function corporatebusiness_inq(Request $request){
-        try{
+
+    public function corporatebusiness_inq(Request $request)
+    {
+        try {
             $cor = new CorporateInquiry;
-            $cor->title=$request->title;
-            $cor->name=$request->name;
-            $cor->contact=$request->contact;
-            $cor->email=$request->email;
-            $cor->address=$request->address;
-            $cor->description=$request->description;
-            $cor->status=0;
-            if($cor->save()){
+            $cor->title = $request->title;
+            $cor->name = $request->name;
+            $cor->contact = $request->contact;
+            $cor->email = $request->email;
+            $cor->address = $request->address;
+            $cor->description = $request->description;
+            $cor->status = 0;
+            if ($cor->save()) {
                 return redirect()->back()->with($this->responseMessage(true, null, "You have successfully added a data."));
             }
-        }catch(Exception $e){
+        } catch (Exception $e) {
             return redirect()->back()->with($this->responseMessage(false, "error", "Please try again!"));
         }
     }
-    public function search(Request $r){
-        if($r->search){
-            $products = Product::where('name', 'LIKE', '%'.$r->search.'%')
-                                ->orWhere('model_no', 'LIKE', '%'.$r->search.'%')
-                                ->orWhere('product_title', 'LIKE', '%'.$r->search.'%')
-                                ->orWhere('product_condition', 'LIKE', '%'.$r->search)
-                                ->paginate(20);
-        }else{
-            $products =false;
-        }
-        $search=$r->search;
-        return view('frontend.search',compact('products','search'));
-    }
-
-    public function wishlist(){
-        $wish = Wishlist::where('customer_id',session()->get('user'));
-        
-        if($wish->count() >0){
-            $wish = $wish->pluck('product_id');
-            $products = Product::whereIn('id',$wish)->get();
-        }else{
+    public function search(Request $r)
+    {
+        if ($r->search) {
+            $products = Product::where('name', 'LIKE', '%' . $r->search . '%')
+                ->orWhere('model_no', 'LIKE', '%' . $r->search . '%')
+                ->orWhere('product_title', 'LIKE', '%' . $r->search . '%')
+                ->orWhere('product_condition', 'LIKE', '%' . $r->search)
+                ->paginate(20);
+        } else {
             $products = false;
         }
-        return view('frontend.wishlist',compact('products'));
+        $search = $r->search;
+        return view('frontend.search', compact('products', 'search'));
     }
 
-    
+    public function wishlist()
+    {
+        $wish = Wishlist::where('customer_id', session()->get('user'));
+
+        if ($wish->count() > 0) {
+            $wish = $wish->pluck('product_id');
+            $products = Product::whereIn('id', $wish)->get();
+        } else {
+            $products = false;
+        }
+        return view('frontend.wishlist', compact('products'));
+    }
+
+
     public function product($id)
     {
-        $categorynam= null;
+        $categorynam = null;
         $data = null;
-        $reviews=ProductReview::where('product_id',$id)->orderBy('rating','DESC')->get();
-        $rating=ProductReview::where('product_id',$id)->avg('rating');
+        $reviews = ProductReview::where('product_id', $id)->orderBy('rating', 'DESC')->get();
+        $rating = ProductReview::where('product_id', $id)->avg('rating');
         $product = Product::find($id);
-        $similar_products=Product::where('id','!=',$id)->where('category_id',$product->category_id)->orderBy('id','DESC')->limit(12)->get();
-        return view('frontend.product',compact('rating','product','data','categorynam','similar_products','reviews'));
+        $similar_products = Product::where('id', '!=', $id)->where('category_id', $product->category_id)->orderBy('id', 'DESC')->limit(12)->get();
+        return view('frontend.product', compact('rating', 'product', 'data', 'categorynam', 'similar_products', 'reviews'));
     }
-
-    
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Models\Role;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\AuthenticationController as auth;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\SubCategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ManufacturerController;
@@ -27,81 +29,96 @@ use App\Http\Middleware\isAdmin;
 */
 
 /********for fronend********/
-// Route::get('/', [FrontEnd::class, 'index'])->name('front.home');
-// Route::get('/games', [FrontEnd::class, 'games'])->name('front.games');
-// Route::get('/categoryss', [FrontEnd::class, 'category'])->name('front.category');
-// Route::get('/aboutus', [FrontEnd::class, 'aboutus'])->name('front.aboutus');
+Route::get('/', [FrontEnd::class, 'index'])->name('front.home');
+Route::get('/games', [FrontEnd::class, 'games'])->name('front.games');
+Route::get('/category', [FrontEnd::class, 'category'])->name('front.category');
+Route::get('/aboutus', [FrontEnd::class, 'aboutus'])->name('front.aboutus');
 
-// Route::get('/corporate-business', [FrontEnd::class, 'corporatebusiness'])->name('front.corporate');
-// Route::post('/corporate-business_inq', [FrontEnd::class, 'corporatebusiness_inq'])->name('front.corporate_inq');
+Route::get('/corporate-business', [FrontEnd::class, 'corporatebusiness'])->name('front.corporate');
+Route::post('/corporate-business_inq', [FrontEnd::class, 'corporatebusiness_inq'])->name('front.corporate_inq');
 
-// Route::get('/contactus', [FrontEnd::class, 'contactus'])->name('front.contactus');
-// Route::get('/c/{id}', [FrontEnd::class, 'categoryview'])->name('front.categoryview');
-// Route::get('/sc/{id}', [FrontEnd::class, 'subcategoryview'])->name('front.subcategoryview');
-// Route::get('/shopview', [FrontEnd::class, 'shopview'])->name('front.shopview');
-// Route::get('/product/{id}', [FrontEnd::class, 'product'])->name('front.product');
-// Route::get('/wishlist', [FrontEnd::class, 'wishlist'])->name('front.wishlist');
-// Route::get('/search', [FrontEnd::class, 'search'])->name('front.search');
-//     /* cart */
-// Route::get('/cart', [CartController::class, 'cartView'])->name('front.cart');
-// Route::get('/addtocart', [CartController::class, 'addToCart'])->name('front.addcart');
-// Route::get('/updatecart', [CartController::class, 'updateCart'])->name('front.updateCart');
-// Route::get('/removecart', [CartController::class, 'removeCart'])->name('front.removeCart');
-//     /* cart */
+Route::get('/contactus', [FrontEnd::class, 'contactus'])->name('front.contactus');
+Route::get('/c/{id}', [FrontEnd::class, 'categoryview'])->name('front.categoryview');
+Route::get('/sc/{id}', [FrontEnd::class, 'subcategoryview'])->name('front.subcategoryview');
+Route::get('/shopview', [FrontEnd::class, 'shopview'])->name('front.shopview');
+Route::get('/product/{id}', [FrontEnd::class, 'product'])->name('front.product');
+Route::get('/wishlist', [FrontEnd::class, 'wishlist'])->name('front.wishlist');
+Route::get('/search', [FrontEnd::class, 'search'])->name('front.search');
+/* cart */
+Route::get('/cart', [CartController::class, 'cartView'])->name('front.cart');
+Route::get('/addtocart', [CartController::class, 'addToCart'])->name('front.addcart');
+Route::get('/updatecart', [CartController::class, 'updateCart'])->name('front.updateCart');
+Route::get('/removecart', [CartController::class, 'removeCart'])->name('front.removeCart');
+/* cart */
+/* checkout */
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('front.checkout');
+Route::get('/guestcheckout', [CheckoutController::class, 'guest_checkout'])->name('front.guestcheckout');
+Route::get('/detailscheckout', [CheckoutController::class, 'details_checkout'])->name('front.detailscheckout');
+Route::post('/checkoutaddress', [CheckoutController::class, 'checkout_address'])->name('front.checkoutaddress');
+Route::get('/finalcheckout', [CheckoutController::class, 'final_checkout'])->name('front.finalcheckout');
+
+Route::get('/shippingcheck', [CheckoutController::class, 'shippingcheck'])->name('front.shippingcheck');
+
+Route::post('/ordersubmit', [CheckoutController::class, 'order_submit'])->name('front.ordersubmit');
+Route::get('/finishorder/{id}', [CheckoutController::class, 'finish_order'])->name('front.finishorder');
 //     /* checkout */
-// Route::get('/checkout', [CheckoutController::class, 'index'])->name('front.checkout');
-// Route::get('/guestcheckout', [CheckoutController::class, 'guest_checkout'])->name('front.guestcheckout');
-// Route::get('/detailscheckout', [CheckoutController::class, 'details_checkout'])->name('front.detailscheckout');
-// Route::post('/checkoutaddress', [CheckoutController::class, 'checkout_address'])->name('front.checkoutaddress');
-// Route::get('/finalcheckout', [CheckoutController::class, 'final_checkout'])->name('front.finalcheckout');
 
-// Route::get('/shippingcheck', [CheckoutController::class, 'shippingcheck'])->name('front.shippingcheck');
 
-// Route::post('/ordersubmit', [CheckoutController::class, 'order_submit'])->name('front.ordersubmit');
-// Route::get('/finishorder/{id}', [CheckoutController::class, 'finish_order'])->name('front.finishorder');
-//     /* checkout */
+/* customer */
+Route::get('/signin', [CustomerController::class, 'signin'])->name('front.signin');
+Route::post('/signin', [CustomerController::class, 'validsignin'])->name('front.signin.valid');
+Route::get('/signup', [CustomerController::class, 'signup'])->name('front.signup');
+Route::post('/signup', [CustomerController::class, 'store'])->name('front.signup.store');
+Route::get('/signout', [CustomerController::class, 'logout'])->name('front.logout');
+
+Route::get('/wishliststore/{id}', [CustomerController::class, 'wishlist'])->name('front.wishlist.store');
+Route::post('/review', [CustomerController::class, 'review'])->name('front.review');
+
+Route::get('/customer_profile', [CustomerController::class, 'cprofile'])->name('front.cprofile');
+Route::post('/customer_profileup', [CustomerController::class, 'profileup'])->name('front.customer.profileup');
+Route::get('/customer_order_list', [CustomerController::class, 'order_list'])->name('front.corderlist');
+Route::get('/customer_invoice/{id}', [CustomerController::class, 'invoice'])->name('front.cinvoice');
+/* customer */
+
+
+
 
 
 //For Banckend
-Route::get('/register', [auth::class,'signUpForm'])->name('register');
-Route::post('/register', [auth::class ,'signUpStore'])->name('register.store');
-Route::get('/', [auth::class,'signInForm'])->name('signIn');
-Route::get('/login', [auth::class,'signInForm'])->name('login');
-Route::post('/login', [auth::class,'signInCheck'])->name('login.check');
-Route::get('/logout', [auth::class,'singOut'])->name('logOut');
+Route::get('/register', [auth::class, 'signUpForm'])->name('register');
+Route::post('/register', [auth::class, 'signUpStore'])->name('register.store');
+Route::get('/adminlogin', [auth::class, 'signInForm'])->name('signIn');
+Route::get('/adminlogin', [auth::class, 'signInForm'])->name('login');
+Route::post('/adminlogin', [auth::class, 'signInCheck'])->name('login.check');
+Route::get('/logout', [auth::class, 'singOut'])->name('logOut');
 
 
-Route::group(['middleware'=>SuperAdminMiddleware::class],function(){
-    Route::prefix('superadmin')->group(function(){
+Route::group(['middleware' => SuperAdminMiddleware::class], function () {
+    Route::prefix('superadmin')->group(function () {
         Route::get('dashboard', function () {
             return view('dashboard');
         })->name('superadmin.dashboard');
-
     });
 });
 
 
-Route::group(['middleware'=>isAdmin::class],function(){
-    Route::prefix('admin')->group(function(){
+Route::group(['middleware' => isAdmin::class], function () {
+    Route::prefix('admin')->group(function () {
         Route::get('dashboard', function () {
             return view('dashboard');
         })->name('admin.dashboard');
-
     });
 });
 
 
 
-Route::resource('category',CategoryController::class);
-Route::resource('subcategory',SubCategoryController::class);
-Route::resource('products',ProductController::class);
-Route::resource('manufacturer',ManufacturerController::class);
-Route::resource('customer',CustomerController::class);
-Route::resource('order',OrderController::class);
+Route::resource('category', CategoryController::class);
+Route::resource('subcategory', SubCategoryController::class);
+Route::resource('products', ProductController::class);
+Route::resource('manufacturer', ManufacturerController::class);
+Route::resource('customer', CustomerController::class);
+Route::resource('order', OrderController::class);
 // Route::resource('corporate',[CorporateSettingController::class,'inq_list']);
-Route::resource('corporate',CorporateInqueryController::class);
+Route::resource('corporate', CorporateInqueryController::class);
 // Route::get('/corporate_setting_inq','inq_list')->name('superadmin.corporate_setting_inq.list');
 // Route::resource('corporate_set',CorporateSettingController::class,['as'=>'superadmin']);
-
-//For Frontend
-// Route::resource('frontend',HeaderviewController::class,['as'=>'superadmin']);
